c6dfc4bc103c2bed314f62b34976fa320828dc56
Leire Abarrategui Martinez <60744058+LeireAbarrategui@users.noreply.github.com>
2021-12-08 11:50:33 +0000

    .

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -1,17 +1,20 @@
+#' @import ExperimentHub
+#' @import utils
 meta <- data.frame(
   Title = c("Control and case samples",
             "Reference panel", 
             "Candidate epimutations"),
-  Description = c(paste0("51 DNA methylation profiling of whole blood samples, 48 controls from 
-                         GSE104812 cohort  and 3 cases from GSE97362"),
+  Description = c(paste0("51 DNA methylation profiling of 
+                          whole blood samples, 48 controls from 
+                          GSE104812 cohort  and 3 cases from GSE97362"),
                   paste0("22 DNA methylation control samples from GSE127824"),
                   paste0("Candidate epimutations in Illumina 450k array")),
   BiocVersion = c(rep("3.13", 3)),
   Genome = c(rep("hg19", 3)),
   SourceType = c(rep("RDA", 3)),
-  SourceUrl = c("https://drive.google.com/drive/folders/1nnaDXPDkSR_LOlgZ_aDldk0UBG8EJ--p?usp=sharing",
-                "https://drive.google.com/drive/folders/1nnaDXPDkSR_LOlgZ_aDldk0UBG8EJ--p?usp=sharing",
-                "https://drive.google.com/drive/folders/1nnaDXPDkSR_LOlgZ_aDldk0UBG8EJ--p?usp=sharing"),
+  SourceUrl = c("shorturl.at/kquwH",
+                "shorturl.at/lE579",
+                "shorturl.at/bdflB"),
   SourceVersion = c(rep("July 2021", 3)),
   Species = "Homo sapiens",
   TaxonomyId = 9606,
